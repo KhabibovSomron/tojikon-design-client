2e7f71a174170a7a8acbaa4760130a091a366450
KhabibovSomron <habibov200@gmail.com>
2023-06-24 16:34:32 +0300

    Добавлены react-router и redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC, lazy } from 'react';
+import { Routes, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Layout = lazy(() => import('./components/Layout'))
+
+interface IAppProps {
+
 }
+
+const App: FC<IAppProps> = () => {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
 
 export default App;
---
+++ src/components/Layout.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react'
+
+
+interface ILayoutProps {
+
+}
+
+const Layout: FC<ILayoutProps> = () => {
+    return (
+        <div className="main">
+            <h1>Layout</h1>
+        </div>
+    )
+}
+
+export default Layout;
